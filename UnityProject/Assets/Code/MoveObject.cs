29f7f26d01d7d1b29763644eff9abf13defa47a5
franklzt <289663346@qq.com>
2018-10-27 22:19:46 +0800

    test simple move logic

---
+++ UnityProject/Assets/Code/MoveObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    public Transform fromObj;
+    public Transform toObj;
+
+    void Start()
+    {
+        StartCoroutine(MovePlayerToEnemy());
+    }
+
+
+    IEnumerator MovePlayerToEnemy()
+    {
+        Animator animator = fromObj.gameObject.GetComponent<Animator>();
+        animator.Play("WALK00_F");
+        while (true)
+        {
+            Vector3 dir = toObj.position - fromObj.position;
+            dir = dir.normalized;
+            if (dir.magnitude < 0.5f)
+            {
+                yield break;
+            }
+            fromObj.position += dir * Time.deltaTime;
+            yield return null;
+        }
+    }
+}
